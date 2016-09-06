0ef19305061241e01e4b8d98643de3c940e8404f
violetta <kobzevavm@gmail.com>
2016-09-06 19:00:25 +0300

    Initial Commit

---
+++ my_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  my_2
+//
+//  Created by Виолетта on 06.09.16.
+//  Copyright © 2016 Виолетта. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
